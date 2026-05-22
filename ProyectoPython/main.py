ef4e71da591f99acd752696beb0539c24dea24ed
Guillermo Rivas <rivas.guillermo6426@gmail.com>
2026-05-22 08:22:07 -0600

    Dashboard cleaned
    Documenté un poco el código, le quité el promedio de datos

--- ProyectoPython/main.py
+++ ProyectoPython/main.py
@@ -1,59 +1,71 @@
+# Imports para interfaz gráfica
 import tkinter as tk
 from tkinter import ttk, messagebox
+
+# Import para leer arduino
 import serial
+
+# Import para graficar
+import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
+# Import para guardar datos
 import csv
 from datetime import datetime
-import numpy as np
+
+# Import para agregar tiempo para inicializar arduino
+from time import sleep
 
 # Intenta conectar con el arduino
 try:
-    arduino = serial.Serial('COM5', 9600, timeout=0.1)
+    arduino = serial.Serial('COM5', 9600, timeout=0)
 except:
     arduino = None
 
+sleep(1.5)
+
 # valor de x visibles en la gráfica
 muestras = 50
-datos_plot0 = np.zeros(muestras)
-datos_plot1 = np.zeros(muestras)
+datos_plot = np.zeros(muestras)
 grabando = False
+
 archivo_csv = None
 escritor_csv = None
 
-
 def update():
-    global datos_plot0, datos_plot1, grabando
+    """
+    Actualiza la ventana en Tkinter
+    """
+    global datos_plot, grabando
     if arduino and arduino.in_waiting > 0:
         try:
-            sum = 0
-            for i in range(20):
-                linea = arduino.readline().decode("utf-8").strip().split(",")
-                analog0 = float(linea[0])
-                sum += analog0
+            linea = arduino.readline().decode("utf-8").strip().split(",")
+            analog = int(linea[0])
 
-            sum /= 20
+            if analog:
+                print(analog)
+                lbl_digitos0.config(text=f'{analog}')
 
-            if sum:
-                print(f"{sum}")
-                lbl_digitos0.config(text=f'{sum:.2f}')
-
-                datos_plot0 = np.roll(datos_plot0, -1)
-                datos_plot0[-1] = sum
-                linea0_plot.set_ydata(datos_plot0)
+                datos_plot = np.roll(datos_plot, -1)
+                datos_plot[-1] = analog
+                linea_plot.set_ydata(datos_plot)
 
                 canvas.draw()
 
                 if grabando:
                     ahora = datetime.now().strftime('%H:%M:%S.%f')[:-3]
-                    escritor_csv.writerow([ahora, sum])
+                    escritor_csv.writerow([ahora, analog])
         except:
             pass
 
-    ventana.after(30, update)  # Re-ejecutar en 30ms
+    ventana.after(1, update)  # Re-ejecutar en 10ms
 
 
 def start():
+    """
+    Inicia a grabar los datos para el csv.
+    """
     global grabando, archivo_csv, escritor_csv
     if not grabando:
         try:
@@ -61,7 +73,7 @@ def start():
             archivo_csv = open(nombre_archivo, "w", newline='')
             escritor_csv = csv.writer(archivo_csv)
 
-            escritor_csv.writerow(["Fecha y Hora", "ADC0", "ADC1"])
+            escritor_csv.writerow(["Hora", "ADC"])
 
             grabando = True
             btn_start.config(state="disabled")
@@ -74,6 +86,9 @@ def start():
 
 
 def stop():
+    """
+    Detiene la grabación del archivo csv.
+    """
     global grabando, archivo_csv
     if grabando:
         grabando = False
@@ -92,15 +107,15 @@ frm_izq = ttk.Frame(ventana, padding=9)
 frm_izq.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
 
 fig, ax = plt.subplots(figsize=(4, 3), dpi=100)
-linea0_plot, = ax.plot(datos_plot0, color='blue', lw=2, label="ADC")
+linea_plot, = ax.plot(datos_plot, color='blue', lw=2, label="ADC")
 
-ax.set_ylim(430, 460)
+ax.set_ylim(-10, 12000)
 ax.set_title("Señal de ADC")
 ax.legend()
 canvas = FigureCanvasTkAgg(fig, master=frm_izq)
 canvas.get_tk_widget().pack(fill=tk.BOTH, expand=True)
 
-frm_der = ttk.Frame(ventana, padding=20, relief="sunken")
+frm_der = ttk.Frame(ventana, padding=30, relief="sunken")
 frm_der.pack(side=tk.LEFT, fill=tk.Y)
 
 ttk.Label(frm_der, text="Valor ADC", font=("Arial", 10, "bold")).pack()
