050ab24b2129bc348c3c355478ad5a0d65aa366b
Guillermo Rivas <rivas.guillermo6426@gmail.com>
2026-06-03 21:05:42 -0600

    Se rehizo por completo el archivo de Arduino
    Ahora recibe un número y envía 2 listas con dicha cantidad de elementos,
    la primera de valores de voltaje, la segunda de diferencia de tiempo en
    microsegundos

--- ProyectoArduino/ProyectoArduino.ino
+++ ProyectoArduino/ProyectoArduino.ino
@@ -6,6 +6,28 @@ void setup(){
 }
 
 void loop(){
-  Serial.println(analogRead(PinIR));
-  delay(40);
+  int numValues = Serial.parseInt();
+  if(numValues>0){
+  int Vadc[numValues];
+  unsigned long T[numValues];
+  unsigned long ref = micros();
+  for(int i = 0; i<numValues;i++){
+    Vadc[i] = analogRead(PinIR);
+    unsigned long timeNow = micros()-ref;
+    T[i] = timeNow;
+    delay(40);
+
+  }
+  for(int i = 0; i< numValues;i++){
+    Serial.print(Vadc[i]);
+    if (i<numValues-1) {Serial.print(",");}
+  }
+  Serial.println();
+
+  for(int i = 0;i<numValues;i++){
+    Serial.print(T[i]);
+    if(i<numValues-1) {Serial.print(",");}
+  }
+  Serial.println();
+  }
 }
